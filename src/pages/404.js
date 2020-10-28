a03255a9e9f8760ab312b8f6b9a7b81bbdaee423
Eurico Catumbela <erickjeremmy@gmail.com>
2020-10-29 00:50:16 +0200

    third commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,9 @@ function NotFoundPage() {
     <Layout>
       <SEO title="404: Not found" />
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h2>This page was not found here
+         
+    </h2>
     </Layout>
   )
 }
